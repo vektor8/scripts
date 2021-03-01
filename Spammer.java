d74e4ca2c4b4fe2f481a946d7e4a959396a49dc2
VictorMosolea <56222237+VictorMosolea@users.noreply.github.com>
2021-03-01 20:29:24 +0200

    Add files via upload

--- Spammer.java
+++ Spammer.java
@@ -20,7 +20,8 @@ public class Spammer {
         options.setExperimentalOption("prefs", prefs);
         System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\chromedriver.exe");
         WebDriver driver = new ChromeDriver(options);
-        driver.get("https://www.facebook.com/messages/t/100027653768597");
+        //in the next line you will insert the link to the person you're trying to spam and it should look like the one below
+        driver.get("https://www.facebook.com/messages/t/100002347463714");
         WebElement acceptButton = driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div/div/div[3]/button[2]"));
         acceptButton.click();
 
@@ -29,13 +30,14 @@ public class Spammer {
         FileReader fr = null;
         try {
             File f = new File("fbpassword.txt");
+            //you should have your facebook access credentials in this txt file: 1st line your email and on the second line your password
             fr = new FileReader(f);
         } catch (Exception e) {
             System.err.println(e.getMessage());
             System.exit(1);
         }
         Scanner sc = new Scanner(fr);
-		usernameBox.sendKeys(sc.nextLine());
+        usernameBox.sendKeys(sc.nextLine());
         passwordBox.sendKeys(sc.nextLine());
         passwordBox.sendKeys(Keys.RETURN);
         TimeUnit.SECONDS.sleep(10);
